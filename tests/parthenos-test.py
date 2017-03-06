c4c1036a598e3b9bf58014decb86e164a8bc596b
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-03-06 13:07:32 +0100

    Basic test added for the current version of matrix

---
+++ tests/parthenos-test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from __future__ import print_function, absolute_import
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+#import pytest
+from tests.config import MATRIX
+from parthenos.core.datatojson import *
+import uuid
+import httpretty
+import requests
+import pandas as pd
+import simplejson
+import json
+
+if __name__ == '__main__':
+    print ('%s' % policies(4))
+    (df, fairfilter) = fair(4) 
+    print ('%s' % fairfilter)
