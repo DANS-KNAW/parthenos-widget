ecfa30f496cc6a41d395e5d9414961bafe874af7
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-05-15 12:31:38 +0200

    Settings added

---
+++ widget/settings.js
@@ -0,0 +1,9 @@
+weburl = "http://zandbak11.dans.knaw.nl/widget";
+polurl = weburl + "/?policy=";
+contentsurl = weburl + '/contents';
+topicsurl = weburl + '/topics';
+apiurl = "http://zandbak11.dans.knaw.nl/widget/?policy=ARHEO%20-%20Arheovest%20Association%20Timisoara";
+apiurl = "http://zandbak11.dans.knaw.nl/widget/?policy=";
+apiurl = "http://zandbak11.dans.knaw.nl/widget/policies";
+apiurl_list = weburl + "/list";
+
