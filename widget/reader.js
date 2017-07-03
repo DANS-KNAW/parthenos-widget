46b3e918788952eb794175475cf0a5776c68b892
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-07-03 12:10:53 +0200

    Reader.js added

---
+++ widget/reader.js
@@ -0,0 +1,81 @@
+d3.json(topicsurl, function(cdata) {
+    var tophtml = '<br><ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">';
+    topichtml = '<br><p>Discipline</p><ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">';
+    var tabnum = 0;
+    for (k in cdata['topics']) {
+        tabnum = tabnum + 1;
+        var k_data = cdata['topics'][k];
+        tophtml = tophtml + '<p><input type="checkbox" value="topic:' + k_data + '" id="input-10a" data-toggle="checkbox-x"> ' + k_data + '</p>';
+        }
+
+    tophtml = tophtml + '</ul>';
+    $(tophtml).appendTo('#topics');
+});
+
+d3.json(apiurl, function(data) {
+    console.log(data);
+    var polhtml = '<br><ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">';
+    for (k in data) {
+        var k_data = data[k];
+        console.log(k_data);
+        for (d in k_data) {
+                console.log(d, k_data[d]);
+                var value = k_data[d];
+                if (!value) {
+                        value = '';
+                }
+                name = "<a href='" + polurl + d + "'>" + d + "</a>";
+                polhtml = polhtml + '<li><b>' + value + '</b>' + name + '</li>';
+        };
+    };
+  polhtml = polhtml + "</ul>";
+  $(polhtml).appendTo('#policies');
+});
+
+d3.json(contentsurl, function(cdata) {
+    var conthtml = '<h4><b>Select your Community</b><div class="tab-pane active" id="godiscipline" style="float:right"><a class="btn btn-primary btnNext">Next</a></div></h4>';
+    var conthtml = '<br><ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">';
+
+    topichtml = '<ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">';
+    var tabnum = 0;
+    var known = new Object();
+    for (k in cdata['contents']) {
+        tabnum = tabnum + 1;
+        var k_data = cdata['contents'][k];
+        if (!k_data) {
+	    conthtml = conthtml + '<p><input name="comm" type="checkbox" value="community:' + k + '" id="input-10a" data-toggle="checkbox-x"> ' + k + '</p>';
+        }
+        else
+        {
+	    var thisval = known[k_data];
+	    if (!thisval) {
+		conthtml = conthtml + '<p><input name="comm" type="checkbox" value="community:' + k_data + '" id="input-10a" data-toggle="checkbox-x"> ' + k_data + '</p>';
+	    };
+            topichtml = topichtml + '<p><input name="disc" type="checkbox" value="discipline:' + k + '" id="input-10a" data-toggle="checkbox-x"> ' + k + '</p>';
+	    known[k_data] = k;
+        }
+    }
+    conthtml = conthtml + "</ul>";
+    topichtml = topichtml + '</ul>';
+    $('#community').append(conthtml);
+
+    $(topichtml).appendTo('#discipline');
+});
+
+$(function() {
+$(document).on("click", '#community input:checkbox', function() {
+    var group = ":checkbox[name='comm']";
+    if($(this).is(':checked')){
+       $(group).not($(this)).attr("checked",false);
+    }
+});
+  });
+
+$(function() {
+$(document).on("click", '#discipline input:checkbox', function() {
+    var group = ":checkbox[name='disc']";
+    if($(this).is(':checked')){
+       $(group).not($(this)).attr("checked",false);
+    }
+});
+  });
