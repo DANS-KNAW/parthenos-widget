d0a4d9db1ff6c235e31e545675a70de54cb91668
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-09-06 17:47:36 +0200

    Matrix changed

--- parthenos/settings/defaults.py
+++ parthenos/settings/defaults.py
@@ -6,6 +6,7 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 BASE_PATH = os.path.abspath(os.path.join(HERE, os.pardir))
 
 matrixfile = "matrix20170515.xlsx"
+matrixfile = "parthenos-matrix-3.2.xlsx"
 MATRIX = os.path.join(BASE_PATH, '../data', matrixfile)
 forbidden = ['POLICY', 'POLICY LINK', 'COUNTRY', 'ORGANISATION', 'ORGANISATION LINK', 'PROJECT']
 majorkey = "POLICY"
