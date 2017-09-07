8db6c4db4eb17d901c357038d90d2e6ce6e286d2
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2017-09-07 11:25:15 +0200

    App updated with new filter

--- app.py
+++ app.py
@@ -59,6 +59,8 @@ def list():
 def gettopics(tabname):
     alltopics = {}
     topickey = "SOCIAL SCIENCE"
+    if tabname:
+	topickey = tabname 
     df = dataloader(topickey)
     columns = []
     for colname in df.columns:
@@ -90,6 +92,33 @@ def read_contents(tabname):
 def main():
     return 'Wizard is running...'
 
+@app.route("/webfilter", methods=['GET', 'POST'])
+def webfilter():
+    newfilterparams = []
+    try:
+         qinput = json.loads( request.data )
+         for name in qinput.keys():
+             newfilterparams.append(str(name))
+    except:
+	return 'no data'
+
+    #newfilterparams = ["community:RESEARCH COMMUNITY","discipline:SOCIAL SCIENCE","topic:CITATION GUIDELINES", "topic:PRIVACY AND SENSITIVE DATA"]
+    params = filtertodict(newfilterparams)
+    discipline = ''
+    for name in params:
+        if params[name] == 'discipline':
+            discipline = name
+	    df = dataloader(discipline)
+    if discipline == '':
+        for name in params: 
+            if params[name] == 'community':
+                discipline = name
+		df = dataloader(discipline)
+
+    outmatrix = mainfilter(df, filtertodict(newfilterparams))
+    data = json.dumps(outmatrix, ensure_ascii=False, sort_keys=True, indent=4)
+    return Response(data,  mimetype='application/json')
+
 @app.route("/contents")
 def webcontents():
     return contents('CONTENTS')
@@ -98,9 +127,23 @@ def webcontents():
 def showlist():
     return list()
 
-@app.route("/topics")
+@app.route("/topics", methods=['GET', 'POST'])
 def webtopics():
-    return gettopics('ARCHAEOLOGY')
+    maindiscipline = 'SOCIAL SCIENCE'
+    if request.data:
+        qinput = json.loads( request.data )
+        thistopic = ''
+        for name in qinput:
+            m = re.match(r'discipline\:(.+)$', name)
+            if m:
+                thistopic = m.group(1)
+        if thistopic:
+            maindiscipline = thistopic
+        else:
+            m = re.match(r'community\:(.+)$', name)
+            if m:
+                maindiscipline = m.group(1)
+    return gettopics(maindiscipline)
 
 if __name__ == '__main__':
     s = datacache(cachedir)
